bcf425593773cfb1d35e0a19fd29adac470ed2be
Roma Kokontsev <Romeo42@ukr.net>
2021-08-08 14:18:18 +0300

    serach movies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,42 @@
+import { lazy, Suspense } from "react";
+import { Route, Switch } from "react-router-dom";
+import NavigationBar from "../NavigationBar/NavigationBar";
+import Container from "../Container/Container";
+import LoaderView from "../Loader/Loader";
 import "./App.css";
 
+const HomeView = lazy(() =>
+  import("../../views/HomeView" /* webpackChunkName: "home-view" */)
+);
+
+const MovieDetailsView = lazy(() =>
+  import("../../views/MovieDetailsView" /* webpackChunkName: "home-view" */)
+);
+
+const MoviesView = lazy(() =>
+  import("../../views/MoviesView" /* webpackChunkName: "movies-view" */)
+);
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container>
+      <NavigationBar />
+      <Suspense fallback={<LoaderView />}>
+        <Switch>
+          <Route path="/movies/:movieId">
+            <MovieDetailsView />
+          </Route>
+
+          <Route path="/movies" exact>
+            <MoviesView />
+          </Route>
+          <Route path="/">
+            <HomeView />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Container>
+  );
 }
 
 export default App;
